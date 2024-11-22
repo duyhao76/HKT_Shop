f25b1f91e73c144bf903e51bb33c6b13da0a2ef7
NGUYENHIEPKA <nguyenhiepka910@gmail.com>
2024-11-22 16:55:54 +0700

    Ka push các entity từ 1-6

---
+++ HKT_SHOP/src/main/java/vn/HKT/entities/UserLevel.java
@@ -0,0 +1,44 @@
+package vn.HKT.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "UserLevel")
+public class UserLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Primary key, auto-generated
+
+    @Column(nullable = false, unique = true, length = 32)
+    private String name; // User level name (Unique, required, maxLength 32)
+
+    @Column(nullable = false, unique = true)
+    private int minPoint; // Minimum point required for this user level (Unique, required)
+
+    @Column(nullable = false)
+    private BigDecimal discount; // Discount for this user level (Required)
+
+    @Column(nullable = false)
+    @Builder.Default
+    private boolean isDeleted = false; // Soft-delete flag (Default: false)
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt; // Auto-generated creation time
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt; // Auto-updated modification time
+}
