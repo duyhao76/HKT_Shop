7b7985ec01791649fe946cc8725c163450417953
longmaychem36 <culitete@gmail.com>
2024-11-28 23:12:23 +0700

    them tempalte guest

---
+++ HKT_SHOP/src/main/java/vn/HKT/controllers/guest/HomeController.java
@@ -0,0 +1,23 @@
+package vn.HKT.controllers.guest;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+@WebServlet(urlPatterns = {"/guest/home"})
+public class HomeController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/views/guest/home.jsp").forward(req, resp);
+	}	
+
+}
